747bbeda3dbac3c772e0c811f7b397ab9ae40a3e
MADASH <ashrafalmayah00@gmail.com>
2023-03-27 13:14:37 +0300

    Adding plate logic to hold ingredients

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -19,6 +19,22 @@ public class ClearCounter : BaseCounter
             if(!player.HasKitchenObject()){
                 //Player isn't carrying anything
                 GetKitchenObject().SetKitchenObjectParent(player);
+            }else{
+                //Player is carrying something
+                if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)){
+                    //Player is carrying a plate
+                    if(plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())){
+                        GetKitchenObject().DestroySelf();
+                    }
+                }else{
+                    //Player isn't carrying a plate but something else
+                    if(GetKitchenObject().TryGetPlate(out plateKitchenObject)){
+                        //Counter is holding a plate
+                        if(plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO())){
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
             }
         }
     }   
