eae6d672bcc547ca461135b0167a1cad0bf01470
MADASH <ashrafalmayah00@gmail.com>
2023-04-02 14:05:57 +0300

    Adding rebinding settings

--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -14,6 +14,21 @@ public class SettingsUI : MonoBehaviour
     [SerializeField]private Button closeButton;
     [SerializeField]private TextMeshProUGUI soundEffectsText;
     [SerializeField]private TextMeshProUGUI musicText;
+    [SerializeField]private Button moveUpButton;
+    [SerializeField]private Button moveDownButton;
+    [SerializeField]private Button moveLeftButton;
+    [SerializeField]private Button moveRightButton;
+    [SerializeField]private Button interactButton;
+    [SerializeField]private Button interactAlternateButton;
+    [SerializeField]private Button pauseButton;
+    [SerializeField]private TextMeshProUGUI moveUpText;
+    [SerializeField]private TextMeshProUGUI moveDownText;
+    [SerializeField]private TextMeshProUGUI moveLeftText;
+    [SerializeField]private TextMeshProUGUI moveRightText;
+    [SerializeField]private TextMeshProUGUI interactText;
+    [SerializeField]private TextMeshProUGUI interactAlternateText;
+    [SerializeField]private TextMeshProUGUI pauseText;
+    [SerializeField]private Transform pressToRebindKeyTransform;
 
     private void Awake() {
         Instance = this;
@@ -25,12 +40,19 @@ public class SettingsUI : MonoBehaviour
         });
         musicButton.onClick.AddListener(() => {
             MusicManager.Instance.ChangeVolume();
-            
+
             UpdateVisual();
         });
         closeButton.onClick.AddListener(() => {
             Hide();
         });
+        moveUpButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Move_Up); });
+        moveDownButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Move_Down); });
+        moveLeftButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Move_Left); });
+        moveRightButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Move_Right); });
+        interactButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Interact); });
+        interactAlternateButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.InteractAlternate); });
+        pauseButton.onClick.AddListener(() => { RebindBinding(GameInput.Binding.Pause); });
     }
 
     private void Start() {
@@ -39,11 +61,20 @@ public class SettingsUI : MonoBehaviour
         UpdateVisual();
 
         Hide();
+        HidePressToRebindKeyTransform();
     }
 
     private void UpdateVisual(){
         soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10f);
         musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10f);
+
+        moveUpText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Up);
+        moveDownText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Down);
+        moveLeftText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Left);
+        moveRightText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Right);
+        interactText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Interact);
+        interactAlternateText.text = GameInput.Instance.GetBindingText(GameInput.Binding.InteractAlternate);
+        pauseText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Pause);
     }
 
     private void GameManager_OnGameUnPaused(object sender, EventArgs e){
@@ -56,5 +87,22 @@ public class SettingsUI : MonoBehaviour
 
     private void Hide(){
         gameObject.SetActive(false);
+    }
+
+    private void ShowPressToRebindKeyTransform(){
+        pressToRebindKeyTransform.gameObject.SetActive(true);
+    }
+
+    private void HidePressToRebindKeyTransform(){
+        pressToRebindKeyTransform.gameObject.SetActive(false);
+    }
+
+    private void RebindBinding(GameInput.Binding binding){
+        ShowPressToRebindKeyTransform();
+
+        GameInput.Instance.RebindBinding(binding, () => {
+            HidePressToRebindKeyTransform();
+            UpdateVisual();
+        });
     }
 }
